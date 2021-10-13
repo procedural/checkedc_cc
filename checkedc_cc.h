ea2dc208e39e6c84df45d0dee86047e052a0204a
procedural <iamvfx@gmail.com>
2021-10-13 05:47:38 +0300

    Create checkedc_cc.h

---
+++ checkedc_cc.h
@@ -0,0 +1,100 @@
+/*
+Copyright(C) 2021 Constantine Tarasenkov (iamvfx@gmail.com). All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+https://github.com/microsoft/checkedc/wiki/Proposed-extension-changes-to-improve-backward-compatibility
+https://github.com/microsoft/checkedc/issues/470
+https://github.com/microsoft/checkedc/blob/d47eaf7f53dd00b6062cc43a2cd1435caf6cc145/include/stdchecked.h
+https://github.com/microsoft/checkedc/blob/d47eaf7f53dd00b6062cc43a2cd1435caf6cc145/spec/bounds_safety/checkedc.tex#L58-L76
+https://github.com/microsoft/checkedc/blob/d47eaf7f53dd00b6062cc43a2cd1435caf6cc145/spec/bounds_safety/core-extensions.tex#L24-L27
+*/
+
+#ifndef __CHECKEDC_CC_H
+#define __CHECKEDC_CC_H
+
+#undef cc_expr
+#undef cc_opt
+#undef cc_ptr_type
+#undef cc_arg
+#undef ptr
+#undef array_ptr
+#undef nt_array_ptr
+#undef checked
+#undef nt_checked
+#undef unchecked
+#undef bounds_only
+#undef where
+#undef dynamic_check
+#undef dynamic_bounds_cast
+#undef assume_bounds_cast
+#undef return_value
+#undef for_any
+#undef itype_for_any
+#undef opaque
+#undef reveal
+#undef and
+
+#ifdef CHECKEDC_CC
+
+#define cc_expr(Expression) ((void)(Expression))
+#define cc_opt(Statement)
+#define cc_ptr_type(Type) Type *
+#define cc_arg(...)
+#define ptr
+#define array_ptr
+#define nt_array_ptr
+#define checked
+#define nt_checked
+#define unchecked
+#define bounds_only
+#define where
+#define dynamic_check
+#define dynamic_bounds_cast
+#define assume_bounds_cast
+#define return_value
+#define for_any
+#define itype_for_any
+#define opaque
+#define reveal
+#define and
+
+#else /* #ifdef CHECKEDC_CC */
+
+#define cc_expr(Expression) Expression
+#define cc_opt(Statement) Statement
+#define cc_ptr_type(Type) <Type>
+#define cc_arg(...) (__VA_ARGS__)
+#define ptr _Ptr
+#define array_ptr _Array_ptr
+#define nt_array_ptr _Nt_array_ptr
+#define checked _Checked
+#define nt_checked _Nt_checked
+#define unchecked _Unchecked
+#define bounds_only _Bounds_only
+#define where _Where
+#define dynamic_check _Dynamic_check
+#define dynamic_bounds_cast _Dynamic_bounds_cast
+#define assume_bounds_cast _Assume_bounds_cast
+#define return_value _Return_value
+#define for_any _For_any
+#define itype_for_any _Itype_for_any
+#define opaque _Opaque
+#define reveal _Reveal
+#define and _And
+
+#endif /* #ifdef CHECKEDC_CC */
+
+#endif /* #ifndef __CHECKEDC_CC_H */
